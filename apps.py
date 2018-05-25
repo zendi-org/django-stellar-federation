edaf2b100b1c6cc87df6a9e623f8099c4427e435
Johannes Innerbichler <j.innerbichler@gmail.com>
2018-05-25 21:40:11 +0200

    changed config app

--- apps.py
+++ apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class StellarFouncationConfig(AppConfig):
+class StellarFoundationConfig(AppConfig):
     name = 'stellar_federation'
     verbose_name = 'Stellar Foundation Sever'
 
